23ce9b15133d2803f8d9fbcd4cedea99fef9c2e8
Andreas Heim <andreas@heim.no>
2013-07-04 15:42:50 +0200

    COLORS

--- clean.sh
+++ clean.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+echo -e "[00;32mKilling all mongod and mongos processes[00m"
 killall mongod mongos
+echo -e "[00;32mRemoving data, log and dump directories[00m"
 rm -r data log dump
